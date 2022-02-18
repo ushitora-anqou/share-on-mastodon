f3c123a7e24a96a08568cb0eb0b07ba147833566
Ushitora Anqou <ushitora@anqou.net>
2022-02-19 00:36:26 +0900

    Encode text

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ browser.browserAction.onClicked.addListener(async (tab) => {
   const st = await browser.storage.sync.get("domain");
   if (st.domain) {
     const domain = st.domain;
-    const text = `${tab.title} ${tab.url}`;
+    const text = encodeURIComponent(`${tab.title} ${tab.url}`);
     const url = `https://${domain}/share?text=${text}`;
     browser.windows.create({
       url: url,
